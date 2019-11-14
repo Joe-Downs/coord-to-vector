264c90695388b6cded4b24bb2215ab5c4b04a6c1
Joe Downs <joedowns502@gmail.com>
2019-11-13 19:51:16 -0500

    coord-to-vector: add take_input() function to prompt user to input relavent information depending on which option they choose

--- coord-to-vector.py
+++ coord-to-vector.py
@@ -15,22 +15,48 @@ def decimal_to_vector():
 def dms_to_vector():
     print("Successfully converted DMS to vector!\n")
 
+degrees_north = None
+minutes_north = None
+seconds_north = None
+degrees_west = None
+minutes_west = None
+seconds_west = None
+full_dms_1 = None
+full_dms_2 = None
+    
+def take_dms_input(is_first):
+    global degrees_north
+    global minutes_north
+    global seconds_north
+    global degrees_west
+    global minutes_west
+    global seconds_west
+    global full_dms_1
+    global full_dms_2
+    degrees_north = int(input("Enter degrees North: "))
+    minutes_north = int(input("Enter minutes North: "))
+    seconds_north = int(input("Enter seconds North: "))
+    #direction = str.upper(str(input("(N)orth or (W)est? ")))
+    if is_first:
+        full_dms_1 = str(degrees_north) + "° " + str(minutes_north) + "' " + str(seconds_north) + '" N '
+    else:
+        full_dms_2 = str(degrees_north) + "° " + str(minutes_north) + "' " + str(seconds_north) + '" N '
+    degrees_west = int(input("Enter degrees West: "))
+    minutes_west = int(input("Enter minutes West: "))
+    seconds_west = int(input("Enter seconds West: "))
+    if is_first:
+        full_dms_1 += str(degrees_west) + "° " + str(minutes_west) + "' " + str(seconds_west) + '" W'
+    else:
+        full_dms_2 += str(degrees_west) + "° " + str(minutes_west) + "' " + str(seconds_west) + '" W'
+
 def take_input(function):
     if function == "dms_to_decimal" or "dms_to_vector" :
         print()
-        degrees_north = int(input("Enter degrees North: "))
-        minutes_north = int(input("Enter minutes North: "))
-        seconds_north = int(input("Enter seconds North: "))
-        #direction = str.upper(str(input("(N)orth or (W)est? ")))
-        full_dms = str(degrees_north) + "° " + str(minutes_north) + "' " + str(seconds_north) + '" N '
-        degrees_west = int(input("Enter degrees West: "))
-        minutes_west = int(input("Enter minutes West: "))
-        seconds_west = int(input("Enter seconds West: "))
-        full_dms += str(degrees_west) + "° " + str(minutes_west) + "' " + str(seconds_west) + '" W'
         if function == "dms_to_decimal":
+            take_dms_input(is_first = True)
             decimal_degrees_north = dms_to_decimal(degrees_north, minutes_north, seconds_north, is_string = True)
             decimal_degrees_west = dms_to_decimal(degrees_west, minutes_west, seconds_west, is_string = True)
-            print(full_dms + " is " + decimal_degrees_north + "° N " + decimal_degrees_west + "° W")
+            print(full_dms_1 + " is " + decimal_degrees_north + "° N " + decimal_degrees_west + "° W")
 
 #Menu allows user 3 options:
 #1 Deg/min/sec to decimal deg
